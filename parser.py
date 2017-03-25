8e84354b037ca5b572c0c79da19fd8cae81c3297
K <wetarmor@gmail.com>
2017-03-26 00:58:29 +0300

    upgraded

--- parser.py
+++ parser.py
@@ -2,9 +2,36 @@ import requests
 from lxml import html
 
 page = requests.get("https://web.nvd.nist.gov/view/vuln/search-results?query=&search_type=all&cves=on")
-tree = html.fromstring(page.content)
+doc = html.fromstring(page.content)
 
-hrefs = tree.xpath('//dt/a')
+links = []
+info = []
 
-for href in hrefs:
-    print(href.get("href"))
+
+def get_cve_links(doc, links):
+    hrefs = doc.xpath('//dt/a')
+
+    for href in hrefs:
+        links.append(href.get("href"))
+
+    return links
+
+
+def get_cve_info(links, info):
+    base_url = "https://web.nvd.nist.gov/view/vuln/"
+
+    for link in links:
+        url = base_url + link
+
+        page = requests.get(url)
+        doc = html.fromstring(page.content)
+
+        overview = doc.xpath('//h4[text()="Overview"]/following-sibling::p/text()')
+        info.append(overview)
+
+    return info
+
+
+links = get_cve_links(doc, links)
+info = get_cve_info(links, info)
+print(info)
