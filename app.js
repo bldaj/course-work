33819ec1496b70244a17e14a55a92578b235af55
K <wetarmor@gmail.com>
2017-06-02 10:50:04 +0300

    updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-require('./routes/index')(app);
+require('./routes')(app);
 
 app.listen(9090, function () {
     console.log('App listening at port 9090');
