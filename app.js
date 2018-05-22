96f8ed28a91f83a7a7b5ff94b2055c223999e52b
k <wetarmor@gmail.com>
2018-05-22 20:47:37 +0300

    updated

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./routes')(app);
 
 app.listen(9090, function () {
