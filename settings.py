3560bb5f187fb69ff15dbcd094574065391e9714
k <wetarmor@gmail.com>
2018-08-25 15:06:24 +0300

    Added settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+settings = {
+    'links': {
+        'xml_base_link': 'https://nvd.nist.gov/feeds/xml/cve/2.0/nvdcve-2.0-',
+        'xml_ending_link': '.xml.gz',
+        'meta_base_link': 'https://nvd.nist.gov/feeds/xml/cve/trans/es/nvdcve-',
+        'meta_ending_link': 'trans.meta'
+    },
+    'files': {
+        'ts_file': 'timestamps.json',
+        'cve_prefix': 'CVE-'
+    }
+}
